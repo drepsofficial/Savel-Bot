a539f9c62c6a687cb7ad0b5e62d1558f386d3376
drepsr <60011650+drepsr@users.noreply.github.com>
2020-03-11 22:08:46 +0300

    Update reboot.js

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
+	var owners = ["439440128495058954","337326216568766464","404935478911959061"];
+
+	if (!owners.includes(message.author.id)) return message.reply('Bu komudu kullanmaya yetkiniz yetmiyor.');
 
     message.channel.send(`Bot yeniden başlatılıyor...`).then(msg => {
     console.log(`BOT: Bot yeniden başlatılıyor...`);
