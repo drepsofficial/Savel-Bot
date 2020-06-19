830c6a16736465882a8f0ec12aac97fea03fce13
drepsr <60011650+drepsr@users.noreply.github.com>
2020-06-19 22:39:47 +0300

    Add files via upload

---
+++ komutlar/bilgi.js
@@ -0,0 +1,32 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Özel mesajlarını kontrol et. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+	const pingozel = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Bot sürümü: 1.0.0' + ayarlar.surum + ' Geliştirici: dreps#0989\n\n_**BOTU EKLEMEK İÇİN:**_\n\ndreps#0989 ulaşın.\n\n**:copyright: 2020 [SAVEL]**');
+    return message.author.sendEmbed(pingozel)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['bot bilgi', 'botbilgi', 'bb', 'botb', 'bbot', 'hakkında', 'bot hakkında', 'bothakkında'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'bilgi',
+  description: 'Bot ile ilgili bilgi verir.',
+  usage: 'bilgi'
+};
