1e24116d3dd7050e81bedf3ec54933b64b36c9ef
drepsr <60011650+drepsr@users.noreply.github.com>
2020-07-30 13:54:47 +0300

    Update yaz.js

--- komutlar/yaz.js
+++ komutlar/yaz.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-  var owners = ["404935478911959061"];
+  var owners = ["404935478911959061","706984527133671464"];
   if (!owners.includes(message.author.id)) return message.reply('Bu komudu kullanmaya yetkiniz yetmiyor.');
   let mesaj = args.slice(0).join(' ');
 if (mesaj.length < 1) return message.reply('Yazmam için herhangi bir şey yazmalısın.');
